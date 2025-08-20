0dcca635e83d2652abbccfed1125ea663cdc3711
FahadAlHossain <fahadalhossain5@gmail.com>
2025-08-21 01:22:41 +0600

    navbar complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,54 @@ import React from "react";
 import Logo from "../../assets/logo.png";
 import { IoSearchOutline } from "react-icons/io5";
 import { IoCartOutline } from "react-icons/io5";
+import { IoMdArrowDropdown } from "react-icons/io";
 import DarkMode from "./DarkMode";
+
+const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Top Rated",
+    link: "/services",
+  },
+  {
+    id: 3,
+    name: "Kids Wear",
+    link: "/#",
+  },
+  {
+    id: 4,
+    name: "Mens Wear",
+    link: "/#",
+  },
+  {
+    id: 5,
+    name: "Electronics",
+    link: "/#",
+  },
+];
+
+const DropDownLinks = [
+  {
+    id: 1,
+    name: "Trending Products",
+    link: "#",
+  },
+  {
+    id: 2,
+    name: "Best Selling",
+    link: "#",
+  },
+  {
+    id: 3,
+    name: "Top Rated",
+    link: "#",
+  },
+];
 
 const Navbar = () => {
   return (
@@ -17,7 +64,7 @@ const Navbar = () => {
           <div className="flex items-center justify-between gap-4">
             <div className="relative group hidden sm:block">
               <input
-                className="bg-white w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-orange-400"
+                className="bg-white w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-orange-400 dark:border-gray-500 dark:bg-gray-800"
                 type="text"
                 placeholder="Search"
               />
@@ -34,10 +81,41 @@ const Navbar = () => {
             </button>
 
             <div>
-                <DarkMode/>
+              <DarkMode />
             </div>
           </div>
         </div>
+      </div>
+      <div className="flex justify-center">
+        <ul className="sm:flex hidden items-center gap-4">
+          {Menu.map((data) => (
+            <li key={data.id}>
+              <a
+                className="inline-block px-4 hover:text-orange-300 duration-200"
+                href={data.link}
+              >
+                {data.name}
+              </a>
+            </li>
+          ))}
+          <li className="group relative cursor-pointer">
+            <a className="flex items-center gap-[2px] py-2" href="#">
+              Trending Items
+              <span className="transition-all duration-200 group-hover:rotate-180">
+                <IoMdArrowDropdown />
+              </span>
+            </a>
+            <div className="absolute z-[9999] hidden group-hover:block w-[150px] rounded-md bg-white p-2 text-black shadow-md">
+              <ul>
+                {DropDownLinks.map((data) => (
+                  <li key={data.id}>
+                    <a className="inline-block w-full rounded-md p-2 hover:bg-orange-300/20" href={data.link}>{data.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   );
