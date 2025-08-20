bfcdd0c7b3c46e32f190244d6d63b68ee566011e
FahadAlHossain <fahadalhossain5@gmail.com>
2025-08-21 03:23:33 +0600

    products added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Hero from './components/Hero/Hero'
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
+import AOS from "aos";
+import "aos/dist/aos.css"
 
 const App = () => {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   return (
     <div>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
+      <Products />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
