a4248bdfc083ab307cc3a6de98230eb35e5f08fd
FahadAlHossain <fahadalhossain5@gmail.com>
2025-08-21 02:29:46 +0600

    hero section done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import img1 from "../../assets/hero/women.png";
 import img2 from "../../assets/hero/shopping.png";
 import img3 from "../../assets/hero/sale.png";
+import Slider from "react-slick";
 
 const ImageList = [
   {
@@ -28,31 +29,55 @@ const ImageList = [
 ];
 
 const Hero = () => {
+  var settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    cssEase: "ease-in-out",
+    pauseOnHover: false,
+    pauseOnFocus: true,
+  };
   return (
     <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
-        <div className="h-[700px] w-[700px] bg-orange-400/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-0">
-
-        </div>
-        <div className="container pb-8 sm:pb-0">
+      <div className="h-[700px] w-[700px] bg-orange-400/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-0"></div>
+      <div className="container pb-8 sm:pb-0">
+        <Slider {...settings}>
+          {ImageList.map((item) => (
             <div>
-                <div className="grid grid-cols-1 sm:grid-cols-2">
-                    <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
-                        <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">Select, Choose, Buy & Make your life easy</h1>
-                        <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, excepturi.</p>
-                        <div>
-                            <button className="bg-gradient-to-r from-orange-400 to-orange-300 hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">Order Now</button>
-                        </div>
-                    </div>
-                    <div className="order-1 sm:order-2">
-                        <div className="relative z-10">
-                            <img className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 object-contain mx-auto" src={img1} alt="" />
-                        </div>
-                    </div>
+              <div className="grid grid-cols-1 sm:grid-cols-2">
+                <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+                  <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
+                    {item.title}
+                  </h1>
+                  <p className="text-sm">
+                    {item.description}
+                  </p>
+                  <div>
+                    <button className="bg-gradient-to-r from-orange-400 to-orange-300 hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
+                      Order Now
+                    </button>
+                  </div>
                 </div>
+                <div className="order-1 sm:order-2">
+                  <div className="relative z-10">
+                    <img
+                      className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-105 lg:scale-120 object-contain mx-auto"
+                      src={item.img}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
-        </div>
+          ))}
+        </Slider>
+      </div>
     </div>
-  ) 
+  );
 };
 
 export default Hero;
