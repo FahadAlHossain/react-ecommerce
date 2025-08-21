06002c8604d9200e544c67c05c1baa05e4345f02
FahadAlHossain <fahadalhossain5@gmail.com>
2025-08-22 01:54:11 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import TopProducts from "./components/TopProducts/TopProducts";
 import Banner from "./components/Banner/Banner";
 import Subscribe from "./components/Subscribe/Subscribe";
 import Testimonials from "./components/Testimonials/Testimonials";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   useEffect(() => {
@@ -29,6 +30,7 @@ const App = () => {
       <Subscribe/>
       <Products />
       <Testimonials/>
+      <Footer></Footer>
     </div>
   );
 };
