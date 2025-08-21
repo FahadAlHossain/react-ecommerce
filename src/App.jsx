f06994f3e492c0e19529f03e30c7ebb4003a556e
FahadAlHossain <fahadalhossain5@gmail.com>
2025-08-21 21:45:08 +0600

    banner added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Products from "./components/Products/Products";
 import AOS from "aos";
 import "aos/dist/aos.css"
 import TopProducts from "./components/TopProducts/TopProducts";
+import Banner from "./components/Banner/Banner";
 
 const App = () => {
   useEffect(() => {
@@ -22,6 +23,7 @@ const App = () => {
       <Hero />
       <Products />
       <TopProducts/>
+      <Banner/>
     </div>
   );
 };
